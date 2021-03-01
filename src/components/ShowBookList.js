a4c7afae81beb0977c5bbe00e355e7a0055cb7dd
geekbim <manyuabim9@gmail.com>
2021-03-01 21:05:21 +0700

    client app

--- src/components/ShowBookList.js
+++ src/components/ShowBookList.js
@@ -1,71 +1,60 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import BookCard from './BookCard';
 
-class ShowBookList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    };
-  }
+function ShowBookList() {
+  const [books, setBooks] = useState([])
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get('http://localhost:8082/api/books')
       .then(res => {
-        this.setState({
-          books: res.data
-        })
+        setBooks(res.data)
       })
       .catch(err =>{
         console.log('Error from ShowBookList');
       })
-  };
+  }, [])
 
+  console.log("PrintBook: " + books);
+  let bookList;
 
-  render() {
-    const books = this.state.books;
-    console.log("PrintBook: " + books);
-    let bookList;
-
-    if(!books) {
-      bookList = "there is no book record!";
-    } else {
-      bookList = books.map((book, k) =>
-        <BookCard book={book} key={k} />
-      );
-    }
-
-    return (
-      <div className="ShowBookList">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <br />
-              <h2 className="display-4 text-center">Books List</h2>
-            </div>
-
-            <div className="col-md-11">
-              <Link to="/create-book" className="btn btn-outline-warning float-right">
-                + Add New Book
-              </Link>
-              <br />
-              <br />
-              <hr />
-            </div>
-
-          </div>
-
-          <div className="list">
-                {bookList}
-          </div>
-        </div>
-      </div>
+  if(!books) {
+    bookList = "there is no book record!";
+  } else {
+    bookList = books.map((book, k) =>
+      <BookCard book={book} key={k} />
     );
   }
+
+  return (
+    <div className="ShowBookList">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <br />
+            <h2 className="display-4 text-center">Books List</h2>
+          </div>
+
+          <div className="col-md-11">
+            <Link to="/create-book" className="btn btn-outline-warning float-right">
+              + Add New Book
+            </Link>
+            <br />
+            <br />
+            <hr />
+          </div>
+
+        </div>
+
+        <div className="list">
+              {bookList}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ShowBookList;
