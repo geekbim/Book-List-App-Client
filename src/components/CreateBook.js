37049554970d32ddee48ebe45abcfd716ec00e48
geekbim <manyuabim9@gmail.com>
2021-03-01 20:58:23 +0700

    client app

--- src/components/CreateBook.js
+++ src/components/CreateBook.js
@@ -1,151 +1,142 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import '../App.css';
 import axios from 'axios';
 
 
-class CreateBook extends Component {
-  constructor() {
-    super();
-    this.state = {
-      title: '',
-      isbn:'',
-      author:'',
-      description:'',
-      published_date:'',
-      publisher:''
-    };
-  }
+function CreateBook() {
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  let history = useHistory()
 
-  onSubmit = e => {
+  const [title, setTitle] = useState('')
+  const [isbn, setIsbn] = useState('')
+  const [author, setAuthor] = useState('')
+  const [description, setDescription] = useState('')
+  const [publishedDate, setPublishedDate] = useState('')
+  const [publisher, setPublisher] = useState('')
+
+  const onSubmit = e => {
     e.preventDefault();
 
     const data = {
-      title: this.state.title,
-      isbn: this.state.isbn,
-      author: this.state.author,
-      description: this.state.description,
-      published_date: this.state.published_date,
-      publisher: this.state.publisher
+      title: title,
+      isbn: isbn,
+      author: author,
+      description: description,
+      published_date: publishedDate,
+      publisher: publisher
     };
 
     axios
       .post('http://localhost:8082/api/books', data)
       .then(res => {
-        this.setState({
-          title: '',
-          isbn:'',
-          author:'',
-          description:'',
-          published_date:'',
-          publisher:''
-        })
-        this.props.history.push('/');
+        setTitle('')
+        setIsbn('')
+        setAuthor('')
+        setDescription('')
+        setPublishedDate('')
+        setPublisher('')
+        history.push("/")
       })
       .catch(err => {
         console.log("Error in CreateBook!");
       })
   };
 
-  render() {
-    return (
-      <div className="CreateBook">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <br />
-              <Link to="/" className="btn btn-outline-warning float-left">
-                  Show BooK List
-              </Link>
-            </div>
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Add Book</h1>
-              <p className="lead text-center">
-                  Create new book
-              </p>
+  return (
+    <div className="CreateBook">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <br />
+            <Link to="/" className="btn btn-outline-warning float-left">
+                Show BooK List
+            </Link>
+          </div>
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Add Book</h1>
+            <p className="lead text-center">
+                Create new book
+            </p>
 
-              <form noValidate onSubmit={this.onSubmit}>
-                <div className='form-group'>
-                  <input
-                    type='text'
-                    placeholder='Title of the Book'
-                    name='title'
-                    className='form-control'
-                    value={this.state.title}
-                    onChange={this.onChange}
-                  />
-                </div>
-                <br />
-
-                <div className='form-group'>
-                  <input
-                    type='text'
-                    placeholder='ISBN'
-                    name='isbn'
-                    className='form-control'
-                    value={this.state.isbn}
-                    onChange={this.onChange}
-                  />
-                </div>
-
-                <div className='form-group'>
-                  <input
-                    type='text'
-                    placeholder='Author'
-                    name='author'
-                    className='form-control'
-                    value={this.state.author}
-                    onChange={this.onChange}
-                  />
-                </div>
-
-                <div className='form-group'>
-                  <input
-                    type='text'
-                    placeholder='Describe this book'
-                    name='description'
-                    className='form-control'
-                    value={this.state.description}
-                    onChange={this.onChange}
-                  />
-                </div>
-
-                <div className='form-group'>
-                  <input
-                    type='date'
-                    placeholder='published_date'
-                    name='published_date'
-                    className='form-control'
-                    value={this.state.published_date}
-                    onChange={this.onChange}
-                  />
-                </div>
-                <div className='form-group'>
-                  <input
-                    type='text'
-                    placeholder='Publisher of this Book'
-                    name='publisher'
-                    className='form-control'
-                    value={this.state.publisher}
-                    onChange={this.onChange}
-                  />
-                </div>
-
+            <form noValidate>
+              <div className='form-group'>
                 <input
-                    type="submit"
-                    className="btn btn-outline-warning btn-block mt-4"
+                  type='text'
+                  placeholder='Title of the Book'
+                  name='title'
+                  className='form-control'
+                  value={title}
+                  onChange={ e => setTitle(e.target.value) }
                 />
-              </form>
-          </div>
-          </div>
+              </div>
+              <br />
+
+              <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='ISBN'
+                  name='isbn'
+                  className='form-control'
+                  value={isbn}
+                  onChange={ e => setIsbn(e.target.value) }
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='Author'
+                  name='author'
+                  className='form-control'
+                  value={author}
+                  onChange={ e => setAuthor(e.target.value) }
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='Describe this book'
+                  name='description'
+                  className='form-control'
+                  value={description}
+                  onChange={ e => setDescription(e.target.value) }
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='date'
+                  placeholder='published_date'
+                  name='published_date'
+                  className='form-control'
+                  value={publishedDate}
+                  onChange={ e => setPublishedDate(e.target.value) }
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  placeholder='Publisher of this Book'
+                  name='publisher'
+                  className='form-control'
+                  value={publisher}
+                  onChange={ e => setPublisher(e.target.value) }
+                />
+              </div>
+
+              <input
+                  type="submit"
+                  className="btn btn-outline-warning btn-block mt-4"
+                  onClick={ onSubmit }
+              />
+            </form>
+        </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default CreateBook;
